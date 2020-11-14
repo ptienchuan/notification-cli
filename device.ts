4aa05d8b08dee935480b1bb656009e0d54d35327
ptienchuan <ptienchuan@gmail.com>
2020-11-14 15:29:14 +0700

    Create drop token command, add tsconfig

--- device.ts
+++ device.ts
@@ -1,6 +1,6 @@
-import * as yargs from 'yargs';
-import * as chalk from 'chalk';
-import { getDeviceTokens } from './services/notification';
+import yargs from 'yargs';
+import chalk from 'chalk';
+import { dropDeviceTokens, getDeviceTokens } from './services/notification';
 import {
   log,
   logTable,
@@ -8,24 +8,51 @@ import {
   logErrorHeading,
   logSuccessHeading,
 } from './utils/log';
+import errorHander from './utils/error-handler';
 
-yargs.command('fetch', 'Get list device tokens', {}, (_) => {
-  logProcessing('Fetching ...');
-  getDeviceTokens()
-    .then((tokens) => {
-      if (!tokens.length) {
-        logErrorHeading('There has 0 tokens');
-        return;
-      }
-
-      logSuccessHeading(`We found ${chalk.red(tokens.length)} tokens`);
-      logTable(tokens);
+yargs.command('fetch', 'Get list device tokens', {}, async (_) => {
+  try {
+    logProcessing('Fetching ...');
+    const tokens = await getDeviceTokens();
+    if (!tokens.length) {
+      logErrorHeading('There has 0 tokens');
       return;
-    })
-    .catch((err) => {
-      logErrorHeading('Opps, we got an error');
-      log(err);
-    });
+    }
+
+    logSuccessHeading(`We found ${chalk.red(tokens.length)} tokens`);
+    logTable(tokens);
+  } catch (error) {
+    errorHander(error);
+  }
 });
+
+yargs.command(
+  'drop',
+  'Drop device token',
+  {
+    token: {
+      alias: 't',
+      array: true,
+      requiresArg: true,
+      string: true,
+      description:
+        'Tokens of device, you can see the list tokens by command fetch',
+    },
+  },
+  async (args) => {
+    try {
+      const { token } = args;
+      if (!token) throw new Error('Nothing to do!');
+
+      logProcessing('Handling ...');
+      const droppedTokens = await dropDeviceTokens(token);
+
+      logSuccessHeading(`${droppedTokens.length} tokens has been dropped:`);
+      droppedTokens.forEach((droppedToken) => log(`  ${droppedToken}`));
+    } catch (error) {
+      errorHander(error);
+    }
+  }
+);
 
 yargs.parse();
--- utils/log.ts
+++ utils/log.ts
@@ -1,9 +1,11 @@
-import * as chalk from 'chalk';
+import chalk from 'chalk';
 
 export const log = console.log;
 export const logTable = console.table;
 
-export const logErrorHeading = (text: unknown) => log(chalk.bgRed.bold(` ${text} `));
+export const logErrorHeading = (text: unknown) =>
+  log(chalk.bgRed.bold(` ${text} `));
 export const logSuccessHeading = (text: unknown) =>
   log(chalk.bgGreen.black.bold(` ${text} `));
 export const logProcessing = (text: unknown) => log(chalk.gray(text));
+export const logError = (text: unknown) => log(chalk.red(text));

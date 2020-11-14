76d94c512d080b4f58c16314454d1401dc8da2c8
Phạm Tiến Chuẩn <ptienchuan@gmail.com>
2020-11-14 21:36:27 +0700

    Create drop token command, add tsconfig (#4)

--- services/notification.ts
+++ services/notification.ts
@@ -9,8 +9,10 @@ interface ExpoToken {
   token: string;
 }
 
-const getDeviceTokens = async () => {
+const getDeviceTokens = async (): Promise<ExpoToken[]> => {
   const { data } = await NotificationService.get('/expoTokens.json');
+  if (!data) return [];
+
   const tokens: ExpoToken[] = Object.keys(data).map((id: string) => ({
     id,
     token: data[id].token,
@@ -19,4 +21,41 @@ const getDeviceTokens = async () => {
   return tokens;
 };
 
-export { getDeviceTokens };
+const dropDeviceTokens = async (
+  dropTokenParameters: string[]
+): Promise<string[]> => {
+  const tokens = await getDeviceTokens();
+  const { notExistTokens, dropTokens } = dropTokenParameters.reduce(
+    (accumulator, currentValue) => {
+      const tokenIndex = tokens.findIndex(
+        ({ token }) => token === currentValue
+      );
+      if (tokenIndex === -1) {
+        accumulator.notExistTokens = accumulator.notExistTokens.concat(
+          currentValue
+        );
+      } else {
+        accumulator.dropTokens = accumulator.dropTokens.concat(
+          tokens[tokenIndex]
+        );
+      }
+      return accumulator;
+    },
+    {
+      notExistTokens: [] as string[],
+      dropTokens: [] as ExpoToken[],
+    }
+  );
+  if (notExistTokens.length) {
+    throw new Error(`These tokens are not exist: ${notExistTokens.join(' ')}`);
+  }
+  await Promise.all(
+    dropTokens.map(({ id }) => {
+      NotificationService.delete(`/expoTokens/${id}.json`);
+    })
+  );
+
+  return dropTokens.map(({ token }) => token);
+};
+
+export { getDeviceTokens, dropDeviceTokens };

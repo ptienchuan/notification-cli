1cac0c857a152325cf660c3a1fcaa96b5bdec2ae
Phạm Tiến Chuẩn <ptienchuan@gmail.com>
2020-11-16 22:34:27 +0700

    Create device pick command (#13)
    * create device pick command

--- handlers/device.ts
+++ handlers/device.ts
@@ -1,41 +1,127 @@
 import { existsSync, writeFileSync, readFileSync } from 'fs';
 import { getDeviceTokens } from '../services/notification';
-import { ExpoToken } from '../types/device';
+import { ExpoToken, DeviceFileData } from '../types/device';
 import { deviceDataFile } from '../configs';
 import { createDataDir } from '../utils';
 
+const readDeviceFileData = (): DeviceFileData => {
+  const fileContent = readFileSync(deviceDataFile).toString();
+  const fileData: DeviceFileData = fileContent
+    ? JSON.parse(fileContent)
+    : { tokens: [], pickedTokens: [] };
+
+  return fileData;
+};
+
+const writeDeviceFileData = (
+  tokens: ExpoToken[],
+  pickedTokens: ExpoToken[]
+): void => {
+  const fileData: DeviceFileData = {
+    tokens,
+    pickedTokens,
+  };
+  writeFileSync(deviceDataFile, JSON.stringify(fileData));
+};
+
+const validateNotExist = (
+  tokenValues?: string[],
+  tokenIndexes?: number[]
+): void => {
+  if (!existsSync(deviceDataFile)) {
+    throw new Error('The list is empty, please fetch');
+  }
+
+  const { tokens } = readDeviceFileData();
+  if (tokenValues && tokenValues.length) {
+    const notExistTokens = tokenValues.filter(
+      (tokenValue) => !tokens.find(({ token }) => token === tokenValue)
+    );
+    if (notExistTokens.length) {
+      throw new Error(
+        `These tokens are not exist: ${notExistTokens.join(' ')}`
+      );
+    }
+  }
+  if (tokenIndexes && tokenIndexes.length) {
+    const notExistIndexes = tokenIndexes.filter(
+      (tokenIndex) => tokenIndex < 0 || tokenIndex >= tokens.length
+    );
+    if (notExistIndexes.length) {
+      throw new Error(
+        `These indexes are not exist: ${notExistIndexes.join(', ')}`
+      );
+    }
+  }
+};
+
 const fetch = async (): Promise<{ added: number; removed: number }> => {
-  const tokens = await getDeviceTokens();
-  let added = tokens.length;
+  let newTokens = await getDeviceTokens();
+  let pickedTokens: ExpoToken[] = [];
+  let added = newTokens.length;
   let removed = 0;
   createDataDir();
 
   if (existsSync(deviceDataFile)) {
-    const fileContent = readFileSync(deviceDataFile).toString();
-    const storedTokens: ExpoToken[] = fileContent
-      ? JSON.parse(fileContent)
-      : [];
-    const intersection = tokens.filter((token) =>
-      storedTokens.find(
-        (storedToken) =>
-          token.token === storedToken.token && token.id === storedToken.id
+    const fileData = readDeviceFileData();
+    const intersection = newTokens.filter(({ token, id }) =>
+      fileData.tokens.find(
+        (storedToken) => token === storedToken.token && id === storedToken.id
       )
     );
-    added = tokens.length - intersection.length;
-    removed = storedTokens.length - intersection.length;
+    added = newTokens.length - intersection.length;
+    removed = fileData.tokens.length - intersection.length;
+    pickedTokens = fileData.pickedTokens;
   }
 
-  writeFileSync(deviceDataFile, JSON.stringify(tokens));
+  writeDeviceFileData(newTokens, pickedTokens);
   return { added, removed };
 };
 
 const list = (): { tokens: ExpoToken[] } => {
   if (!existsSync(deviceDataFile)) return { tokens: [] };
 
-  const fileContent = readFileSync(deviceDataFile).toString();
-  const tokens: ExpoToken[] = fileContent ? JSON.parse(fileContent) : [];
+  const { tokens } = readDeviceFileData();
 
   return { tokens };
 };
 
-export default { fetch, list };
+const pick = (
+  isAll: boolean,
+  pickTokenValues?: string[],
+  pickTokenIndexes?: number[]
+): number => {
+  validateNotExist(pickTokenValues, pickTokenIndexes);
+  const fileData = readDeviceFileData();
+
+  if (isAll) {
+    const notYetPickedTokens = fileData.tokens.filter(
+      ({ token, id }) =>
+        !fileData.pickedTokens.find(
+          (pickedToken) => pickedToken.token === token && pickedToken.id === id
+        )
+    );
+    writeDeviceFileData(fileData.tokens, [
+      ...fileData.pickedTokens,
+      ...notYetPickedTokens,
+    ]);
+    return notYetPickedTokens.length;
+  }
+
+  const tokenWillBePicked = fileData.tokens.filter(({ token, id }, index) => {
+    const isChoosen =
+      (pickTokenIndexes && pickTokenIndexes.includes(index)) ||
+      (pickTokenValues && pickTokenValues.includes(token));
+    const isPicked = fileData.pickedTokens.find(
+      (pickedToken) => pickedToken.token === token && pickedToken.id === id
+    );
+    return isChoosen && !isPicked;
+  });
+  writeDeviceFileData(fileData.tokens, [
+    ...fileData.pickedTokens,
+    ...tokenWillBePicked,
+  ]);
+  return tokenWillBePicked.length;
+};
+
+export default { fetch, list, pick };
--- types/device.ts
+++ types/device.ts
@@ -2,3 +2,8 @@ export interface ExpoToken {
   id: string;
   token: string;
 }
+
+export interface DeviceFileData {
+  tokens: ExpoToken[];
+  pickedTokens: ExpoToken[];
+}
